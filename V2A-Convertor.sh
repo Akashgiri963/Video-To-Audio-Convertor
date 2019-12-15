b26614de5f5d72e9f80681633c2dc0971f86c1b3
4NONYM0U5 H4CK3R <37062817+Akashgiri963@users.noreply.github.com>
2019-12-15 16:23:58 +0530

    Create V2A-Convertor.sh

---
+++ V2A-Convertor.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+green='\e[32m'
+yellow='\e[33m'
+magneto='\e[36m'
+reset='\e[0m'
+
+function banner(){
+
+ echo -e "\n$green"
+ echo "
+       	       ╔────────────────────────────────────────────────╗
+       	       |    Video To Audio Convertor Version - 1.0.1    |
+       	       |          Developed By :- Akash Giri            |
+       	       ┖────────────────────────────────────────────────┙
+ "
+ figlet -c -k -f small Video2Audio|lolcat 
+ figlet -c -k -f small Convertor|lolcat
+ echo
+}
+
+function ext(){
+ i=1
+ echo
+ for f in *.mp4; do
+  echo -e  "$magneto [$i].$yellow $f $reset"
+  (( i++ )); done
+ echo
+}
+
+banner
+echo -e "$magneto \nEnter the directory [ After Home Directory ] : $green\c"
+read dir
+echo -e "$yellow \nChanging directory...$reset"
+sleep 1
+target=~/$dir; cd $target
+ext
+
+echo -e "$magneto \nEnter File Name : $green \c"
+read fn
+dir=$target/$fn
+echo
+echo -e "$yellow \nCoverting...$magneto\n"
+sleep 1
+
+ffmpeg -i $dir -c:a libmp3lame $target/$fn.mp3
+echo -e "\nSuceessfully Converted... :)$reset\n"
+figlet -c -k -f small Thank You|lolcat
